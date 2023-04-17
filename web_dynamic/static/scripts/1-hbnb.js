904cae60c9916ad8ceb083fdc50db295c5d34e4c
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-04-17 12:21:48 -0700

    a JavaScript script (static/scripts/1-hbnb.js),script is executed only when DOM is loaded

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$('document').ready(function () {
+
+  /*****************************************************
+    display list of checkboxes clicked
+   *****************************************************/
+  let amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+});
