0618a4e2ad3a23cf2fe7789ac8d575d100b521f2
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-04-19 09:40:14 -0700

    readding js files

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -3,15 +3,14 @@ $( document ).ready(function () {
   /*****************************************************
     display list of checkboxes clicked
    *****************************************************/
-  let ls_amen = [];
+  let ls_amen = {};
   $('input[type=checkbox]').change (function () {
-    let name = $(this).attr('data-name');
-      if ($(this).is(':checked')) {
-	ls_amen.push(name);
-      } else {
-	ls_amen = ls_amen.filter(amen => amen !== name);
-      }
-    $('.amenities h4').text(ls_amen.join(', '));
+    if ($(this).is(':checked')) {
+      ls_amen[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete ls_amen[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(ls_amen).join(', '));
   });
 
   /*******************************************************
@@ -44,5 +43,24 @@ $( document ).ready(function () {
 	}
       }
     });
+
+  /*******************************************************
+    populate Places from frontend, instead of backend jinja;
+    filter places displayed based on amenity checkboxed list
+   *******************************************************/
+  $('button').click(function () {
+    $('article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({'amenities': Object.keys(ls_amen)}),
+      contentType: 'application/json',
+      success: function (data) {
+	for (let i = 0; i < data.length; i++) {
+	  $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
+	}
+      }
+    });
+  });
 
 });
