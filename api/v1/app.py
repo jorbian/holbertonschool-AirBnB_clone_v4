4c44d05bbdc1e81f5235b1eafc03cf6fc32b8eb9
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-04-19 19:50:11 -0700

    everything

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,108 +1,31 @@
 #!/usr/bin/python3
-"""
-Flask App that integrates with AirBnB static HTML Template
-"""
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, render_template, url_for
-from flask_cors import CORS, cross_origin
-from flasgger import Swagger
+"""appy appers"""
+from flask import Flask, make_response
 from models import storage
+from api.v1.views import app_views
 import os
-from werkzeug.exceptions import HTTPException
+from flask_cors import CORS
 
-# Global Flask Application Variable: app
 app = Flask(__name__)
-swagger = Swagger(app)
-
-# global strict slashes
-app.url_map.strict_slashes = False
-
-# flask server environmental setup
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
-# Cross-Origin Resource Sharing
-# cors = CORS(app, origins="0.0.0.0")
-# cors = CORS(app, resources={r'/*': {'origins': host}})
-# cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
-
-
-# begin flask page rendering
-@app.teardown_appcontext
-def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
-    storage.close()
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
 def handle_404(exception):
-    """
-    handles 404 errors, in the event that global error handler fails
-    """
+    """handles 404 scenario (page not found)"""
     code = exception.__str__().split()[0]
-    description = exception.description
-    message = {'error': description}
-    return make_response(jsonify(message), code)
+    message = {"error": "Not found"}
+    return make_response(message, code)
 
 
-@app.errorhandler(400)
-def handle_404(exception):
-    """
-    handles 400 errros, in the event that global error handler fails
-    """
-    code = exception.__str__().split()[0]
-    description = exception.description
-    message = {'error': description}
-    return make_response(jsonify(message), code)
-
-
-@app.errorhandler(Exception)
-def global_error_handler(err):
-    """
-        Global Route to handle All Error Status Codes
-    """
-    if isinstance(err, HTTPException):
-        if type(err).__name__ == 'NotFound':
-            err.description = "Not found"
-        message = {'error': err.description}
-        code = err.code
-    else:
-        message = {'error': err}
-        code = 500
-    return make_response(jsonify(message), code)
-
-'''
-@app.after_request
-def add_cors_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
-    response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
-    response.headers.add('Access-Control-Allow-Headers', 'Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-    return response;
-'''
-
-def setup_global_errors():
-    """
-    This updates HTTPException Class with custom error function
-    """
-    for cls in HTTPException.__subclasses__():
-        app.register_error_handler(cls, global_error_handler)
+@app.teardown_appcontext
+def close_storage(exception):
+    """close storage instance"""
+    storage.close()
 
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App
-    """
-    # initializes global error handling
-    setup_global_errors()
-    # start Flask app
-    app.run(host=host, port=port)
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = os.environ.get("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
